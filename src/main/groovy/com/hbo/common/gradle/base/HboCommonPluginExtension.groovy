8ddfeaafa9698befbce393dbd053e4a14db023d5
John Bramlett <jbramlett@3street.net>
2018-01-07 20:16:45 -0500

    Updating build info

--- src/main/groovy/com/hbo/common/gradle/base/HboCommonPluginExtension.groovy
+++
@@ -1,9 +0,0 @@
-package com.hbo.common.gradle.base
-
-/**
- * Created by jbramlet on 5/2/17.
- */
-class HboCommonPluginExtension {
-    double minCoverage = 0.70
-    boolean spockTesting = true
-}
